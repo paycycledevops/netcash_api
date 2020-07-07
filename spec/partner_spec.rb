f6ac3a2a5492602511047419d5d1d499407e86a1
Jono Booth <jonobth@gmail.com>
2020-07-07 13:10:56 +0200

    fix:

--- spec/partner_spec.rb
+++ spec/partner_spec.rb
@@ -47,4 +47,28 @@ RSpec.describe NetcashApi::Partner do
       binding.pry
     end
   end
+
+  it do
+    VCR.use_cassette 'validate_service_key_xml' do
+      response = NetcashApi::Client.partner.call(:validate_service_key, xml: xml_string)
+      binding.pry
+    end
+  end
+end
+
+def xml_string
+'</ValidateServiceKey>
+<MethodParameters>
+  <request>
+   <SoftwareVendorKey>24ade73c-98cf-47b3-99be-cc7b867b3080</SoftwareVendorKey>
+    <MerchantAccount>52624757488</MerchantAccount>
+      <ServiceInfoArray>
+        <ServiceInfoArray0>
+         <ServiceId>1</ServiceId>
+         <ServiceKey>4724c261-dad5-4a64-852c-9b88a67b3c50</ServiceKey>
+        </ServiceInfoArray0>
+       </ServiceInfoArray>
+  </request>
+ </MethodParameters>
+</ValidateServiceKey>'
 end
