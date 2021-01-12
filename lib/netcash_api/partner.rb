91352ea9f2af43db19abc0a71bd5b65b8c9737bc
Jono Booth <jonobth@gmail.com>
2021-01-12 13:43:45 +0200

    Partner update

--- lib/netcash_api/partner.rb
+++ lib/netcash_api/partner.rb
@@ -1,9 +1,4 @@
 module NetcashApi
-  class Partner
-    class << self
-      def method_missing(end_point, params, &block)
-        NetcashApi::Client.send(self.to_s.demodulize.underscore.downcase).call(end_point, xml: params)
-      end
-    end
+  class Partner < ClientWrapper
   end
 end
